894ed5c61af0b03806036bfd0b25aba8251d13f8
Yash Bhatia <mr.yash12345@gmail.com>
2020-10-17 21:58:45 +0530

    Final Scientific calculator

---
+++ app/src/main/java/com/example/finalcalculator/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.finalcalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent i =new Intent(MainActivity.this,ScientificActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1000);
+    }
+}
